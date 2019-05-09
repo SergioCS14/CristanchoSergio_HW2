7e2510177723b898f7705c0a6913dbf1da503bb6
SergioCS14 <se.cristancho@uniandes.edu.co>
2019-05-09 04:23:50 +0000

    Ejercicio 3: Edificio terminado. Faltan los plots.

--- Edificio.cpp
+++ Edificio.cpp
@@ -34,6 +34,7 @@ float a_3(int i){
 }
 
 int main(){
+    /* Primer parte */
     float omega = sqrt(k/m);
     u_1  = new float[n];
     u_2  = new float[n];
@@ -75,6 +76,57 @@ int main(){
     outfile1.close();
     outfile2.close();
     outfile3.close();
+    
+    /* Segunda parte: máximos */
+
+    std::ofstream outfilem1;
+    outfilem1.open("MaximoP1.dat");
+    std::ofstream outfilem2;
+    outfilem2.open("MaximoP2.dat");
+    std::ofstream outfilem3;
+    outfilem3.open("MaximoP3.dat");
+    for(int j=0; j<100; j++){
+        omega = (0.2+j*(3.0-0.2)/100)*sqrt(k/m);
+        /* Máximos */
+        float u_1max = 0;
+        float u_2max = 0;
+        float u_3max = 0;
+        /* Condiciones iniciales */
+        u_1[0] = 0;
+        u_2[0] = 0;
+        u_3[0] = 0;
+        v_1[0] = 0;
+        v_2[0] = 0;
+        v_3[0] = 0;
+        /* Iteración: Leap-Frog */
+        for(int i=1; i<n; i++){
+            float v_1h = v_1[i-1]+a_1(i-1, omega)*dt/2;
+            float v_2h = v_2[i-1]+a_2(i-1)*dt/2;
+            float v_3h = v_3[i-1]+a_3(i-1)*dt/2;
+            u_1[i] = u_1[i-1]+v_1h*dt;
+            u_2[i] = u_2[i-1]+v_2h*dt;
+            u_3[i] = u_3[i-1]+v_3h*dt;
+            v_1[i] = v_1h + a_1(i, omega)*dt/2;
+            v_2[i] = v_2h + a_2(i)*dt/2;
+            v_3[i] = v_3h + a_3(i)*dt/2;
+            if (abs(u_1[i]) > u_1max){
+                u_1max = abs(u_1[i]);
+            }
+            if (abs(u_2[i]) > u_2max){
+                u_2max = abs(u_2[i]);
+            }
+            if (abs(u_3[i]) > u_3max){
+                u_3max = abs(u_3[i]);
+            }
+        }
+        outfilem1<<omega<<","<<u_1max<<std::endl;
+        outfilem2<<omega<<","<<u_2max<<std::endl;
+        outfilem3<<omega<<","<<u_3max<<std::endl;
+    }
+    outfilem1.close();
+    outfilem2.close();
+    outfilem3.close();
+    
     return 0;
 }
     
