fc7ba656cb9bffb8968f7ea4229868aad62a3a42
Sergio Enrique Cristancho Sanchez <UANDES\se.cristancho@fisicaez12223l.ad.uniandes.edu.co>
2019-05-09 14:04:10 -0500

    Creado el makefile. Debub de Edificio antes de bono.

---
+++ hw2.mk
@@ -0,0 +1,10 @@
+Resultados_hw2.pdf : Resultados_hw2.tex 2_Signal.pdf 2_FourierSignal.pdf 2_Espectrogramas.pdf 2_Temblor.pdf 2_FourierTemblor.pdf 2_EspectrogramaTemblor.pdf 3_Maximos.pdf
+	pdflatex Resultados_hw2.tex
+2_Signal.pdf 2_FourierSignal.pdf 2_Espectrogramas.pdf 2_Temblor.pdf 2_FourierTemblor.pdf 2_EspectrogramaTemblor.pdf : Fourier.py signal.dat signalSuma.dat temblor.txt
+	python Fourier.py
+3_Maximos.pdf : Plots_hw2.py 3_MaximoP1.dat 3_MaximoP2.dat 3_MaximoP3.dat
+	python Plots_hw2.py
+3_MaximoP1.dat 3_MaximoP2.dat 3_MaximoP3.dat : a.out 
+	./a.out
+a.out : Edificio.cpp
+	g++ -std=c++11 Edificio.cpp
