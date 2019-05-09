30a48d67484d9b7e355cd843052a08f60c807cb9
SergioCS14 <se.cristancho@uniandes.edu.co>
2019-05-09 02:44:17 +0000

    Ejercicio 3: COndiciones iniciales.

--- Edificio.cpp
+++ Edificio.cpp
@@ -2,7 +2,31 @@
 #include <fsstream>
 #include <math.h>
 
+float F( float O, float T){
+    return sin(O*T);
+}
+
 int main(){
+    /* Constantes */
+    int n = 100;
+    float m = 1000;
+    float k = 2000;
+    float omega = sqrt(k/m);
+    /* Variables */
+    float u_1 [n];
+    float u_2 [n];
+    float u_3 [n];
+    float v_1 [n];
+    float v_2 [n];
+    float v_3 [n];
+    /* Condiciones iniciales */
+    u_1[0] = 0;
+    u_2[0] = 0;
+    u_3[0] = 0;
+    v_1[0] = 0;
+    v_2[0] = 0;
+    v_3[0] = 0;
+    /* Iteración: Leap-Frog */
     
     return 0;
 }
